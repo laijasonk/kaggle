f5a06b7d458476bc546037c4c46fb02cc5a34458
Jason Lai <jason.lai@tessella.com>
2018-12-06 16:23:49 -0600

    Breaking down a sample code
    As a starting point, decided to attempt to break down an example of a
    kernel found in Kaggle. Took a beginners kernel, placed it on a py file
    and am now looking at each line at a time to learn the components.

--- humpback_whale_identification/bin/test.py
+++ humpback_whale_identification/bin/test.py
@@ -1,44 +1,203 @@
 #!/usr/bin/env python3
 
-#import the necessary libraries
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt
-import matplotlib.image as mplimg
-from matplotlib.pyplot import imshow
-import zipfile as zf
-import os
-import csv
-import gc
-import operator
-import random
+# source: amneves_humpkeras-whale-identification
+
+import sys, os
+import skimage.io
+import skimage.transform
+import math
+from glob import glob
+from tqdm import tqdm
+from PIL import Image, ImageDraw
+import tensorflow as tf
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Input, GlobalAveragePooling2D
 from sklearn.model_selection import train_test_split
-from numpy import array
-from numpy import argmax
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from random import shuffle
-#from IPython.display import Image
-from pathlib import Path
+from imgaug import augmenters as iaa
+from tensorflow.keras.metrics import categorical_accuracy, categorical_crossentropy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential, load_model, Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import metrics
+from tensorflow.keras.optimizers import Adam 
+from tensorflow.keras import backend as K
+import tensorflow.keras as keras
 
-from keras import layers
-from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
-from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
-from keras.models import Model
-from keras.preprocessing import image
-from keras.utils import layer_utils
-from keras.utils.data_utils import get_file
-from keras.applications.imagenet_utils import preprocess_input
-#from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
-from keras.utils import plot_model
-from keras.utils import np_utils
-import keras.backend as K
-from keras.models import Sequential
-from keras import optimizers
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
 
-#load the training data
-trainData = pd.read_csv("../data/train.csv")
+# general pathes
+PATH = './'
+TRAIN = '../data/train/'
+TEST = '../data/test/'
+LABELS = '../data/train.csv'
+SAMPLE = '../data/sample_submission.csv'
 
-#See what is in the data
-print( trainData.sample(5) )
+# basic info about train data
+label_df = pd.read_csv( LABELS )
+submission_df = pd.read_csv( SAMPLE )
+print( label_df.head() )
+print( label_df['Id'].describe() )
+
+# Display the most frequent ID (without counting new_whale)
+label_df['Id'].value_counts()[1:16].plot(kind='bar')
+
+# check how many unique labels there are
+n_classes = label_df['Id'].nunique()
+
+# set the image shape for later
+img_shape = (224,224,3)
+
+print( n_classes )
+
+#def get_pad_width(im, new_shape, is_rgb=True):
+#    pad_diff = new_shape - im.shape[0], new_shape - im.shape[1]
+#    t, b = math.floor(pad_diff[0]/2), math.ceil(pad_diff[0]/2)
+#    l, r = math.floor(pad_diff[1]/2), math.ceil(pad_diff[1]/2)
+#    if is_rgb:
+#        pad_width = ((t,b), (l,r), (0, 0))
+#    else:
+#        pad_width = ((t,b), (l,r))
+#    return pad_width
+#
+#
+#def pad_and_resize(image_path, dataset):
+#    img = skimage.io.imread( [ '../input/', str(dataset), '/', str(image_path) ] )
+#    pad_width = get_pad_width(img, max(img.shape))
+#    padded = np.pad(img, pad_width=pad_width, mode='constant', constant_values=0)
+#    resized = skimage.transform.resize(padded, (224,224))
+#    
+#    return resized
+
+data = pd.read_csv( LABELS )
+target_dummies = pd.get_dummies(label_df['Id'])
+train_label = target_dummies.columns.values
+y_train = target_dummies.values
+
+sys.exit()
+
+train_dataset_info = []
+for name, labels in zip(data['Image'], y_train):
+    train_dataset_info.append({
+        'path':os.path.join(TRAIN, name),
+        'labels': labels})
+train_dataset_info = np.array(train_dataset_info)
+
+train_ids, test_ids, train_targets, test_target = train_test_split(
+    data['Image'], data['Id'], test_size=0.1)
+
+class data_generator:
+    
+    def create_train(dataset_info, batch_size, shape, n_labels, augument=True):
+        assert shape[2] == 3
+        while True:
+            random_indexes = np.random.choice(len(dataset_info), batch_size)
+            batch_images = np.empty((batch_size, shape[0], shape[1], shape[2]))
+            batch_labels = np.zeros((batch_size, n_labels))
+            for i, idx in enumerate(random_indexes):
+                image = data_generator.load_image(
+                    dataset_info[idx]['path'], shape)   
+                if augument:
+                    image = data_generator.augment(image)
+                batch_images[i] = image
+
+                batch_labels[i] = dataset_info[idx]['labels']
+            yield batch_images, batch_labels
+            
+
+    def load_image(path, shape):
+        img = skimage.io.imread(path)
+        resized = skimage.transform.resize(img,  (shape[0], shape[1]))
+        resized = resized / 255
+        return resized
+
+            
+    def augment(image):
+        augment_img = iaa.Sequential([
+            iaa.OneOf([
+                iaa.Affine(rotate=0),
+                iaa.Affine(rotate=90),
+                iaa.Affine(rotate=180),
+                iaa.Affine(rotate=270),
+                iaa.Fliplr(0.5),
+                iaa.Flipud(0.5),
+            ])], random_order=True)
+        
+        image_aug = augment_img.augment_image(image)
+        return image_aug
+
+# create visualization datagen
+vis_datagen = data_generator.create_train(
+    train_dataset_info, 5, img_shape, n_classes, augument=False)
+
+images, labels = next(vis_datagen)
+fig, ax = plt.subplots(1,5,figsize=(25,5))
+for i in range(5):
+    ax[i].imshow(images[i])
+
+batchsize = 64
+# create train datagen
+train_datagen = data_generator.create_train(
+    train_dataset_info[train_ids.index], batchsize, img_shape, n_classes, augument=True)
+
+validation_generator = data_generator.create_train(
+    train_dataset_info[test_ids.index], 256, img_shape, n_classes, augument=False)
+
+def gen_graph(history, title):
+    plt.plot(history.history['categorical_accuracy'])
+    plt.plot(history.history['val_categorical_accuracy'])
+    plt.title('Accuracy ' + title)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'validation',], loc='upper left')
+    plt.show()
+    plt.plot(history.history['categorical_crossentropy'])
+    plt.plot(history.history['val_categorical_crossentropy'])
+    plt.title('Loss ' + title)
+    plt.ylabel('MLogLoss')
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+STEPS = 512
+epochs = 15
+
+def create_model(input_shape, n_out):
+    base_model = MobileNet(input_shape=input_shape, include_top=False, weights=None, classes=n_out)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Activation('relu')(x)
+    x = Dropout(0.2)(x)
+    x = Dense(1024)(x)
+    x = Activation('relu')(x)
+    x = Dense(n_out)(x)
+    logits = Activation('softmax')(x)
+    
+    for layer in base_model.layers:
+        layer.trainable = True
+        
+    return Model(inputs=base_model.input, outputs=logits)
+
+model = create_model(input_shape=img_shape, n_out=n_classes)
+
+model.compile(optimizer=Adam(lr=0.002), loss='categorical_crossentropy',
+              metrics=[categorical_crossentropy, categorical_accuracy])
+model.summary()
+
+callbacks = [
+    ReduceLROnPlateau(monitor='val_categorical_accuracy', factor=0.5, patience=5,
+                      min_delta=0.005, mode='max', cooldown=3, verbose=1)
+]
+
+hist = model.fit_generator(
+    train_datagen, steps_per_epoch=STEPS, epochs=epochs, verbose=1,
+    validation_data=next(validation_generator),
+    callbacks = callbacks)
+
+gen_graph(hist, "Mobile Net, lr 1e-4")
